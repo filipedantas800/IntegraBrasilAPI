d16466a2bf8e0d10766fad313ab12276797def09
filipedantas800 <filipetecnico800@hotmail.com>
2023-06-06 20:53:53 -0300

    Criação das Models e Dtos.

--- Dtos/ResponseGenerico.cs
+++ Dtos/ResponseGenerico.cs
@@ -1,7 +1,12 @@
-﻿namespace IntegraBrasilAPI.Dtos
+﻿using System.Dynamic;
+using System.Net;
+
+namespace IntegraBrasilAPI.Dtos
 {
     public class ResponseGenerico<T> where T : class
     {
-        public HttpStatusCode
+        public HttpStatusCode CodigoHttp { get; set; }
+        public T? DadosRetorno { get; set; }
+        public ExpandoObject? ErroRetorno { get; set; }
     }
 }
